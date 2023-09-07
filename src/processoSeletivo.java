fe0beec080af614885819b39f0e2bfb770c28565
Luiz Henrique <81529238+IkkyLuiz@users.noreply.github.com>
2023-09-07 16:09:16 -0300

    criando a classe processo seletivo.

---
+++ src/processoSeletivo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class processoSeletivo {
+    public static void main(String[] args){
+    analisarCandidato(1900.0);
+    analisarCandidato(2200.0);
+    analisarCandidato(2000.0);
+
+    }
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.00;
+        if(salarioBase > salarioPretendido){
+        System.out.println("Ligar para o candidato");
+        }else if(salarioBase==salarioPretendido)
+        System.out.println("Enviar contraproposta");
+        else
+        System.out.println("Aguardando demais candidatos");
+    }
+}
